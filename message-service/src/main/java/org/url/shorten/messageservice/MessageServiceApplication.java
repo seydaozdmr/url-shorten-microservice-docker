144f16541a1a160bb594d9be93c272b1b94584de
Seyda Özdemir <seydaozdemir07@gmail.com>
2022-09-16 16:47:40 +0300

    enabled rabbit mq

--- message-service/src/main/java/org/url/shorten/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/org/url/shorten/messageservice/MessageServiceApplication.java
@@ -1,9 +1,11 @@
 package org.url.shorten.messageservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MessageServiceApplication {
 
     public static void main(String[] args) {
